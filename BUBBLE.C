6500dfcfd05c21681024447f2439c9f99c5f2e7c
Dhanushratti <dratti@student.gitam.edu>
2026-01-29 14:54:16 +0530

    Update BUBBLE.C

--- BUBBLE.C
+++ BUBBLE.C
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int a[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = 7;
-    int i, j, temp;
+    int n;
 
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (a[j] > a[j + 1]) {
-                temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 
     printf("Sorted array:\n");
-    for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
-
