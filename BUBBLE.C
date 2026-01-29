3ed6eb721b7a8f4cadcfc36c5abd15fd52952a62
Dhanushratti <dratti@student.gitam.edu>
2026-01-29 15:22:24 +0530

    Update BUBBLE.C

--- BUBBLE.C
+++ BUBBLE.C
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
 int main() {
     int n;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     int arr[n];
-
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
@@ -29,3 +25,4 @@ int main() {
 
     return 0;
 }
+
