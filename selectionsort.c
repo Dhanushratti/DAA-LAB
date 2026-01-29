d12b48b4bb65f963c012c19741a53bedc5258f21
Dhanushratti <dratti@student.gitam.edu>
2026-01-29 14:59:53 +0530

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,42 +1,36 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
+int main() {
+    int n;
 
-    for (i = 0; i < n - 1; i++) {
-        minIndex = i;
-        for (j = i + 1; j < n; j++) {
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
         if (minIndex != i) {
-            temp = arr[i];
+            int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
     }
-}
 
-int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i;
-
-    printf("Original array: ");
-    for (i = 0; i < n; i++) {
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-    printf("\n");
-
-    selectionSort(arr, n);
-
-    printf("Sorted array:   ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
 
     return 0;
 }
-
